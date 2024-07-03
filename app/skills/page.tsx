25185653ea11819797e2cad48adc3b06b6c34389
Delight Sheriff <delightsheriff@gmail.com>
2024-07-03 03:05:09 +0100

    projects page:fix-2

--- app/skills/page.tsx
+++ app/skills/page.tsx
@@ -45,8 +45,8 @@ const SkillCard: React.FC<SkillCardProps> = ({ name, imageSrc }) => (
 
 const Skills: React.FC = () => {
   return (
-    <section className="w-full py-12 md:py-24 lg:py-32 ">
-      <div className="container max-w-5xl px-4 md:px-6">
+    <section className="w-full border border-red-800 py-12 md:py-24 lg:py-32 ">
+      <div className="container max-w-5xl px-0 md:px-6">
         <div className="grid gap-12 lg:grid-cols-2">
           <div className="space-y-6">
             <h2 className="text-3xl font-bold tracking-tighter text-text-light dark:text-text-dark sm:text-4xl md:text-5xl">
